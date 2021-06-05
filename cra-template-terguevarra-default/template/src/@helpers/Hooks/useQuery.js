28eb49dda390e8ed0b1481a07446290a90110afc
terguevarra <ruther.guevarra@gmail.com>
2021-06-05 17:52:05 +0800

    Added useQuery hooks

---
+++ cra-template-terguevarra-default/template/src/@helpers/Hooks/useQuery.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+function useQuery(){
+  return new URLSearchParams(useLocation().search);
+}
+
+export default useQuery;
